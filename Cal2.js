6bfa3a2524773b6fce2e1533eb19ab830f34098d
ayomide2020 <ben@gmail.com>
2020-02-13 16:50:19 +0100

    initial commit

---
+++ Cal2.js
@@ -0,0 +1,11 @@
+<script type="text/javascript">
+          function onoff(){
+            currentvalue.innerHTML = document.getElementById('onoff').value;
+            if(currentvalue.innerHTML == "Off"){
+              document.getElementById("onoff").value="On";
+            }else{
+              document.getElementById("onoff").value="on/off";
+            }
+          }
+</script>
+          
